067a9a1a66c359afcf8a3f17777f9bb7f482ff26
xAksssenov <aks.kirill32@gmail.com>
2023-12-15 13:13:50 +0300

    rework navigation with consts

--- web-react/src/components/Navbar/index.tsx
+++ web-react/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { CONTACTS_ROUTE, HOME_ROUTE, PRODUCTS_ROUTE, REVIEWS_ROUTE } from '../../routes/configs';
 
 interface NavbarProps {
     isAuth: boolean;
@@ -7,15 +8,15 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ isAuth, setIsAuth }) => {
     const click = () => {
-        setIsAuth(!isAuth)
+        setIsAuth((prev) => !prev)
     }
 
     return (
         <div className="navbar">
-            <Link to="/">Главная</Link>
-            <Link to="/products">Товары</Link>
-            <Link to="/reviews">Отзывы</Link>
-            <Link to="/contacts">Контакты</Link>
+            <Link to={HOME_ROUTE}>Главная</Link>
+            <Link to={PRODUCTS_ROUTE}>Товары</Link>
+            <Link to={REVIEWS_ROUTE}>Отзывы</Link>
+            <Link to={CONTACTS_ROUTE}>Контакты</Link>
             <button onClick={click}>{isAuth ? 'Выйти' : 'Войти'}</button>
         </div>
     )
