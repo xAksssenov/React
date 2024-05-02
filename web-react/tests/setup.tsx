f0c459a5faf0c0be1c45b474cdf97ad067b5cb8f
xAksssenov <aks.kirill32@gmail.com>
2024-05-02 13:15:46 +0300

    feat: start to do tests

---
+++ web-react/tests/setup.tsx
@@ -0,0 +1,19 @@
+import { cleanup, render } from '@testing-library/react';
+import { afterEach } from 'vitest';
+import '@testing-library/jest-dom';
+
+afterEach(() => {
+  cleanup();
+});
+
+function customRender(ui: React.ReactElement, options = {}) {
+  return render(ui, {
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+}
+
+export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
+
+export { customRender as render };
